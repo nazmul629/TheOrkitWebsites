65177ddb25335bd2aa82680c4e0ef973140746fa
nazmul629 <nazmulhossain.qnh@gmail.com>
2024-08-16 16:19:23 +0600

    This commit display the porduct form backend to home.html and front page of website

--- theorkit/viwes.py
+++ theorkit/viwes.py
@@ -1,5 +1,10 @@
 # from django.http import render
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request,'home.html')
+    products =  Product.objects.all().filter(is_avilable=True)
+    context = {
+        'products' : products,
+    }
+    return render(request,'home.html',context)
