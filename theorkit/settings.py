39dfc83f8adefddc1a6dda7cf618a1250d919fde
nazmul629 <nazmulhossain.qnh@gmail.com>
2024-08-16 11:15:10 +0600

    in this section updating the settings.py and projects urls.py file  for the meadia file uploading

--- theorkit/settings.py
+++ theorkit/settings.py
@@ -118,6 +118,13 @@ STATICFILES_DIRS =[
     'theorkit/static',
 ]
 
+
+#Medoa Files configurations
+
+MEDIA_URL = 'media/' 
+MEDIA_ROOT = BASE_DIR/'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
