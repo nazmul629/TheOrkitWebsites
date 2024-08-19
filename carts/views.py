404cf34ea30334b53eee7459d01acb639f34e2b9
nazmul629 <nazmulhossain.qnh@gmail.com>
2024-08-20 02:03:57 +0600

    prooduct variations template and product variation modele constract after all gert the insttance of variations of for single product

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from  store.models import Product
+from  store.models import Product,Variation
 from .models import Cart, CartItem
 from django.http import HttpResponse
 
@@ -16,7 +16,32 @@ def _cart_id(request):
 
 def add_cart(request,product_id):
 
-    product = Product.objects.get(id=product_id)
+    product = Product.objects.get(id=product_id) #get the product
+    product_variation = []
+
+    # traditional 
+    # color = request.POST['color']
+    # size = request.POST['size']
+
+    if request.method == 'POST':
+        #  Dynamic collet multiple atribute
+        
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+   
+            
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+
+                # product_variation.append(variation)
+                print(variation)
+
+            except:
+                pass
+
+
+
     
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
--- store/models.py
+++ store/models.py
@@ -19,18 +19,34 @@ class Product (models.Model):
     
     def get_url(self):
         return reverse('product_detail',args=[self.category.slug, self.slug])
+    
 
-variation_category_choice= (
+
+
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice=(
     ('color','color'),
     ('size','size'),
 )
 
 class Variation(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variation_category  = models.CharField(max_length=20, choices= variation_category_choice)
-    variation_value = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    variation_category  = models.CharField(max_length=20,choices=variation_category_choice)
+    variation_value = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
-    create_date =models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now=True)
 
-    def __uniode__(self):
-        return self.product
+    objects = VariationManager()
+    
+
+    def __str__(self):
+        return self.variation_value
+
+    
