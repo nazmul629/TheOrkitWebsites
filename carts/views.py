e93602eb962fb7315801b5856ddb4cb50383c6d5
nazmul629 <nazmulhossain.qnh@gmail.com>
2024-08-19 22:00:02 +0600

    solve  few bug

--- carts/views.py
+++ carts/views.py
@@ -70,7 +70,7 @@ def remove_cart(request,product_id):
     return redirect('cart')
 
 
-def cart(request, total = 0 , quantity = 0, total_cart_item = 0, cart_items = None):
+def cart(request, total = 0 , quantity = 0, total_cart_item = 0, cart_items = None,fee = 0,grand_total=0 ) :
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active = True)
